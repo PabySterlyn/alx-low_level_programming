e070b2e75f5c2ed8e93cb52813fbf1d7fcc87bd9
PabySterlyn <dandoh2015@gmail.com>
2023-03-17 16:26:20 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,9 +9,9 @@ int main(void)
 {
 	char i;
 
-	for (i = 'a' ; i <= 'z' ; i++)
+	for (i = 'a'; i <= 'z'; i++)
 		if (i != 'q' && i != 'e')
 			putchar(i);
-	putchar("\n");
+	putchar('\n');
 	return (0);
 }
